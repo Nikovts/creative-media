8b784b2e90d773717d171fed6eb98f4246b0b1c5
Nikolay Nikolov <nikolay.nikolov@ioio.tv>
2023-05-22 16:10:27 +0300

    initial commit

---
+++ creative-media/src/components/Card.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="card">
+    <img class="card-image" :src="imageUrl"/>
+    <div class="card-data">
+      <div class="card-details">
+        <h3 class="card-title">{{item.title}}</h3>
+        <ul>
+          <li class="card-points" v-for="point,i in item.points" :key="i">
+            {{point}}
+          </li>
+        </ul>
+        <p class="card-description">{{ item.description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(['item'])
+const imageUrl = new URL(`../assets/${props.item.img}.jpg`, import.meta.url).href
+</script>
+
+<style lang="scss">
+  .card {
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    background: #FFFFFF;
+    border-radius: 8px;
+  }
+
+  .card-image {
+    width: 11.46rem;
+    height: 21rem;
+    border-radius: 8px 0px 0px 8px;
+  }
+
+  .card-data {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 2.5rem 1.875rem 2.125rem 2.125rem;
+    gap: 1.875rem;
+
+    width: 15.875rem;
+    height: 21rem;
+  }
+  .card-details {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 0.75rem;
+
+    .card-title {
+      font-family: 'Poppins';
+      font-weight: 700;
+      font-size: 1.5rem;
+      line-height: 2rem;
+      font-feature-settings: 'salt' on, 'liga' off;
+      color: #353844;
+    }
+
+    ul {
+      width: 11.81rem;
+      list-style: none;
+      margin-block-start: 0;
+      padding-inline-start: 0;
+    }
+
+    li::before {
+      content: "•";
+      color: #D2AD81;
+      margin-right: 0.5rem;
+    }
+
+    .card-points {
+      width: 11.81rem;
+      font-family: 'Nunito Sans';
+      font-style: normal;
+      font-weight: 700;
+      font-size: 0.9375rem;
+      line-height: 1.625rem;
+      font-feature-settings: 'salt' on, 'liga' off;
+      color: #353844;
+    }
+
+    .card-description {
+      width: 11.81rem;
+      font-family: 'Nunito Sans';
+      font-style: normal;
+      font-weight: 600;
+      font-size: 0.9375rem;
+      line-height: 1.5rem;
+      font-feature-settings: 'salt' on, 'liga' off;
+      color: #353844;
+    }
+
+  }
+
+</style>
---
+++ creative-media/src/components/MainPage.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="main">
+    <header>
+      <h1 class="title">We make creative media that <span>adds value</span></h1>
+    </header>
+    <section>
+      <Card v-for="item,i in data" :key="i" :item="item"/>
+    </section>
+    <footer>
+      <span>Get STARTED</span>
+    </footer>
+  </div>
+</template>
+
+<script setup>
+import Card from "./Card.vue";
+import { ref } from 'vue'
+import dataDB from "../assets/data.json";
+
+const data = ref(dataDB)
+</script>
+
+<style lang="scss" scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 6.75rem 5.26rem;
+}
+
+header {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 45.62rem;
+  height: 6.875rem;
+}
+
+section {
+  margin: 5rem 0;
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  gap: 2.5rem 1.875rem;
+}
+
+footer {
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  padding: 0px 3.625rem;
+  width: 19.69rem;
+  height: 3.75rem;
+  background: #1B264F;
+  border-radius: 0.75rem;
+
+  span {
+    font-weight: 700;
+    font-size: 1.0625rem;
+    line-height: 3.75rem;
+    text-align: center;
+    letter-spacing: 0.2em;
+    text-transform: uppercase;
+    color: #FFFFFF;
+    cursor: pointer;
+  }
+}
+.title{
+  font-style: normal;
+  font-weight: 700;
+  font-size: 3.5rem;
+  line-height: 98%;
+
+  text-align: center;
+  letter-spacing: -0.01em;
+
+  color: #1B264F;
+
+  span {
+    font-style: normal;
+    font-weight: 700;
+    font-size: 3.5rem;
+    line-height: 98%;
+    color:#506BCA;
+  }
+}
+
+</style>
